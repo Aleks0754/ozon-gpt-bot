63ba06f25dd4332c4eebd10f381e216026d43da3
Aleksandr <aleksandr@MacBook-Air-Aleksandr.local>
2025-04-22 22:29:11 +0700

    исправление Телеграм бота

--- services/telegram_notify.py
+++ services/telegram_notify.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+CHAT_ID = os.getenv("TELEGRAM_USER_ID")
 
 def send_telegram_message(message: str):
     if not TELEGRAM_TOKEN or not CHAT_ID:
